3cb741a5ddd8c0fe24ac3235dd5bbf8c2e86c2e6
Jeferson Schlarski <jefersonunik@gmail.com>
2025-01-25 23:56:25 -0300

    adicionando configurações de linguagem

---
+++ lang/en/errors.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Errors Language Messages
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used to display various error messages
+    | throughout the application. You can modify these messages according
+    | to your application's requirements to provide better feedback to the user.
+    |
+    */
+
+    'creation_error' => ':attribute could not be created.',
+    'create_token_error' => 'User auth token generation failed.',
+    'update_error' => 'Failed to change :attribute.',
+    'logout_error' => 'Logout failed.',
+    'not_found' => ':attribute not found.',
+    'unauthorized' => 'You are not authorized to perform this action.',
+    'different_value_required' => 'The new :attribute needs to be different from the current one',
+];
